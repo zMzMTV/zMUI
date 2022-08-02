a9338d70adc131a0a7e6f46dee12602e005e5066
Yassine Azem <yazem@iMac-de-Admin.local>
2022-08-02 16:09:36 +0200

    creating repo and set up project with ts, eslint, prettier, husky and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,tsx,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
